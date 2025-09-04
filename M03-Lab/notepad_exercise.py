a1c59785c0c34382e1918c06aae1afcfed4aa3aa
Marc Robertson <robertsonmarc@myocu.oak.edu>
2025-09-04 09:19:32 -0500

    Stack functions
    Builds, and manipulates the stack for output.

--- M03-Lab/notepad_exercise.py
+++ M03-Lab/notepad_exercise.py
@@ -11,4 +11,25 @@
 
 from stack_implementation import push, pop, peek, is_empty, size
 
+def add_word(word):
+    push(word)
+
+def show():
+    # Display the stack in order of items being added
+    # We can't access the internal stack directly, so we use a temporary stack
+
+    # Temporary list to hold words as we pop them to the stack
+    temp = []
+    while not is_empty():
+        temp.append(pop())
+
+    # Reverse temp to restore original insertion order
+    temp.reverse()
+
+    # Prints space separated sentence
+    print(" ".join(temp))
+
+    # Print items back into stack
+    for word in temp:
+        push(word)
 
