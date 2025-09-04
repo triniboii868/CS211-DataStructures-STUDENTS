2b03ed431e5a10ec4ab83885e32078320d17f3a0
Marc Robertson <robertsonmarc@myocu.oak.edu>
2025-09-04 09:06:22 -0500

    Functions
    Imports functions defined in the stack_implementation module.

---
+++ M03-Lab/notepad_exercise.py
@@ -0,0 +1,14 @@
+#Stack Notepad Exercise
+# ----------------------
+# Building a simple command-line notepad using a stack
+# - Type a word to add it to your document (push onto the stack)
+# - Type 'UNDO' to remove the last word (pop from the stack) 
+# - Type 'SHOW' to display the current document contents (display stack contents)
+# Implementation methods from the stack_implementatin.py file. Do not use your own method.
+# Complete the function below to make the stack work.
+#-----------------------
+
+
+from stack_implementation import push, pop, peek, is_empty, size
+
+
